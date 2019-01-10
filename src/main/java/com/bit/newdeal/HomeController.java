97791f61e03b6af5276799f2c0a1f85c13c5bcea
root9003 <hermes9003@naver.com>
2019-01-10 15:13:48 +0900

    no message

--- src/main/java/com/bit/newdeal/HomeController.java
+++ src/main/java/com/bit/newdeal/HomeController.java
@@ -1,30 +1,21 @@
 package com.bit.newdeal;
 
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.bit.newdeal.dto.memberDto;
-import com.bit.newdeal.service.testService;
-
 @Controller
 public class HomeController {
-	/*
-	  @Autowired
-	  private testService test;
-	*/
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model, HttpSession session) {
-		/*System.out.println(test.test());*/
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
--- src/main/java/com/bit/newdeal/dao/testDao.java
+++
@@ -1,8 +0,0 @@
-package com.bit.newdeal.dao;
-
-import com.bit.newdeal.dto.memberDto;
-
-public interface testDao {
-
-	public memberDto test();
-}
